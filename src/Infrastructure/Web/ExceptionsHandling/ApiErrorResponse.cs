2c990ff7422bcfe7028d2ce157dc364691f1ad96
dmitriy.litichevskiy <dmitry.litichevskiy@byndyusoft.com>
2017-04-05 16:35:23 +0500

    Primitives for WebApi development were added

---
+++ src/Infrastructure/Web/ExceptionsHandling/ApiErrorResponse.cs
@@ -0,0 +1,29 @@
+﻿namespace Byndyusoft.Dotnet.Core.Infrastructure.Web.ExceptionsHandling
+{
+    using System;
+    using System.Text;
+
+    public class ApiErrorResponse : ExceptionData
+    {
+        public string Message { get; private set; }
+
+        public ApiErrorResponse(string message, Exception exception) : base(exception)
+        {
+            Message = message;
+        }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(Message) == false)
+                builder.AppendLine(Message);
+
+            var baseInformation = base.ToString();
+            if (string.IsNullOrWhiteSpace(baseInformation) == false)
+                builder.AppendLine(baseInformation);
+
+            return builder.ToString();
+        }
+    }
+}
