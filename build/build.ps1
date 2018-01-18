a842dbd8eefca0950f70fb01717fa38b77d65102
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2018-01-18 23:02:03 +0500

    .NET Core runtime for Cake.CoreCLR was updated

--- build/build.ps1
+++ build/build.ps1
@@ -39,7 +39,7 @@ $CakePath = Get-ChildItem -Filter Cake.dll -Recurse | Sort-Object -Descending | 
 if (!(Test-Path $DOTNET_PATH)) 
 {
 	Write-Host "Downloading runtime..."
-	powershell -NoProfile -ExecutionPolicy unrestricted -Command "&([scriptblock]::Create((Invoke-WebRequest -useb 'https://dot.net/v1/dotnet-install.ps1'))) -Version 1.0.8 -SharedRuntime -InstallDir $CLI_DIR -NoPath"
+	powershell -NoProfile -ExecutionPolicy unrestricted -Command "&([scriptblock]::Create((Invoke-WebRequest -useb 'https://dot.net/v1/dotnet-install.ps1'))) -Version 1.0.9 -SharedRuntime -InstallDir $CLI_DIR -NoPath"
 }
 
 Write-Host "Running build script..."
